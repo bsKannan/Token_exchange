92113f94ce3e4e40dbff20ed94be3825954e2d8c
bsKannan <36512419+bsKannan@users.noreply.github.com>
2018-03-08 17:21:22 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -2,39 +2,33 @@ pragma solidity ^0.4.0;
 contract ERC20
 {
     
-        string public name;
-        string public symbol;
-        uint256 public decimals;
-        uint256 public totalSupply;
-        function ERC20()
-        {
-            name="TOKEN";
-            symbol="$$";
-            decimals=18;
-            totalSupply=1000;
-        }
-    struct su
+    string public name;
+    string public symbol;
+    uint256 public decimals;
+    uint256 public totalSupply;
+    function ERC20()
     {
-        address to;
-        uint256 value;
-        uint256 amount;
+        name="TOKEN";
+        symbol="$$";
+        decimals=0;
+        totalSupply=1000;
     }
-    mapping(address=>su)map;
+    mapping(address=>uint256)map;
 
     function transfer(address to,uint256 value)returns(bool)
     {
-       require(totalSupply>value);
-       map[msg.sender].amount-=value;
-       map[to].value+=value;
+       require(map[msg.sender]>=value);
+       map[msg.sender]-=value;
+       map[to]+=value;
        return true;
     }
     function balanceOf(address owner) public constant returns(uint256)
     {
-        return map[owner].amount;
+        return map[owner];
     }
     function mint(uint256 amount) 
     {
         require(totalSupply>amount);
-        map[msg.sender].amount=+amount;
+        map[msg.sender]=amount;
     }
 }
